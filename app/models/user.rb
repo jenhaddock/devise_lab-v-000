e43ac1df7b5e223dd1feb268ae2e74a63767566b
Jennifer Haddock <jentx123@hotmail.com>
2017-10-11 01:24:32 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
-         validates :password, length: { minimum: 8 }
+         validates :password, length: { minimum: 8 }, allow_blank: true
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|

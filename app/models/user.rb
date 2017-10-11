d595f978013942d81328d6b2ce78c44d3dcc4961
Jennifer Haddock <jentx123@hotmail.com>
2017-10-11 01:14:02 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook],
+         :omniauthable, :omniauth_providers => [:facebook]
          validates :password, length: { minimum: 8 }
 
   def self.from_omniauth(auth)
